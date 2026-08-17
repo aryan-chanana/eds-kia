4f3bf545d49dae5220d072717877a1ace2a4fe83
Neeta Marne <neetam26@gmail.com>
2026-08-17 15:36:53 +0530

    Updated js file  and removed cards related extra files

--- blocks/startJourney/startJourney.js
+++ blocks/startJourney/startJourney.js
@@ -1,23 +1,37 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+  const cards = [...block.children];
+
+  cards.forEach((card) => {
+    card.classList.add('start-journey-card');
+
+    const cells = [...card.children];
+
+    const imageCell = cells[0];
+    const textCell = cells[1];
+
+    // Image
+    if (imageCell) {
+      imageCell.classList.add('start-journey-card-image');
+
+      const image = imageCell.querySelector('img');
+      if (image) {
+        image.classList.add('start-journey-card-icon');
+      }
+    }
+
+    // Text content
+    if (textCell) {
+      textCell.classList.add('start-journey-card-content');
+
+      const heading = textCell.querySelector('h2, h3, h4');
+
+      if (heading) {
+        heading.classList.add('start-journey-card-title');
+      }
+
+      textCell.querySelectorAll('p').forEach((paragraph) => {
+        paragraph.classList.add('start-journey-card-description');
+      });
+    }
   });
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-  block.replaceChildren(ul);
 }
